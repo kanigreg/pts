0f9993882e2a27a164e141acdfaed9ba1dff94e8
kanigreg <kanigreg@gmail.com>
2025-11-09 14:43:48 +0500

    fix: erease dir before clone repo

--- boot.sh
+++ boot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/.local/share/
-git clone -f git@github.com:kanigreg/pts.git ~/.local/share/pts >/dev/null
+rm -fr ~/.local/share/pts
+git clone git@github.com:kanigreg/pts.git ~/.local/share/pts >/dev/null
 
 bash ~/.local/share/pts/install.sh
