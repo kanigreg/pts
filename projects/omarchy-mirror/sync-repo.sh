cf280d1997874714476f76e0f258fe9aaad3db8b
kanigreg <kanigreg@gmail.com>
2025-11-11 14:46:02 +0500

    WIP

--- projects/omarchy-mirror/sync-repo.sh
+++ projects/omarchy-mirror/sync-repo.sh
@@ -1,5 +1,6 @@
 docker run --rm -it \
+  -v "$(pwd)/bin:/root/bin" \
   -v "$(pwd)/pacman/pacman.conf:/etc/pacman.conf" \
   -v /srv/pacman/omarchy/os/x86_64:/var/cache/pacman/pkg \
   archlinux \
-  bash -c "pacman -Syw $(pacman -Slq omarchy)"
+  bash
