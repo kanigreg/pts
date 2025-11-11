8775853ca577a58467cf90874027d15b6229023d
kanigreg <kanigreg@gmail.com>
2025-11-11 15:15:48 +0500

    WIP

--- projects/omarchy-mirror/init.sh
+++ projects/omarchy-mirror/init.sh
@@ -1,6 +1,6 @@
 docker run --rm -it \
   -v "$(pwd)/bin:/root/bin" \
   -v "$(pwd)/pacman/pacman.conf:/etc/pacman.conf" \
-  -v /srv/pacman/omarchy/os/x86_64:/var/cache/pacman/pkg \
+  -v /srv/http/pacman/omarchy/os/x86_64:/var/cache/pacman/pkg \
   archlinux \
   /root/bin/sync
